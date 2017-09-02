741a9e07b776c943eb34e1c22195674c3146d65d
Tanish Goel <30473669+TanishGoel@users.noreply.github.com>
2017-09-02 14:39:19 +0530

    Create watermelon.cpp
    Solution to code forces problem set 4A

---
+++ watermelon.cpp
@@ -0,0 +1,15 @@
+// 4A codeforces
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i;
+  cin >> i;
+  if(i <= 2 || i % 2 == 1){
+    cout << "NO";
+  }
+  else{
+    cout << "YES"<< endl;
+  }
+}
